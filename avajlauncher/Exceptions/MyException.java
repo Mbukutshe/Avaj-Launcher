a127a75864a80d5e7724780cbbf593896b0f419b
Khanyisa MBUKUTSHE <kmbukuts@c4r5s11.wethinkcode.co.za>
2020-10-03 15:43:09 +0200

    fixed product

--- avajlauncher/Exceptions/MyException.java
+++ avajlauncher/Exceptions/MyException.java
@@ -1,7 +1,7 @@
 package avajlauncher.Exceptions;
 
 public class MyException extends Exception {
-    private static final long serialVersionUID = -1928219242566779452L;
+    private static final long serialVersionUID = -192L;
 
     public MyException()
     { 
